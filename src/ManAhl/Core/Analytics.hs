9fd96ca8f1199c9c5aa2dc46b9789ea52b1ba157
Fadil Mokhchane <fadil.mokhchane@gmail.com>
2017-08-26 03:53:01 +0200

    Improve complexity of mkCdf from O(4n) to O(3n)

--- src/ManAhl/Core/Analytics.hs
+++ src/ManAhl/Core/Analytics.hs
@@ -9,18 +9,21 @@ import ManAhl.Core.Types
 mkCdf :: PDF -> CDF
 mkCdf (PDF xs) = CDF $ Map.fromAscList $ reverse $ check $ foldl go [] xs
   where
-    go :: [(Int, Double)] -> (Int, Double) -> [(Int, Double)]
-    go [] x = [x]
-    go (y:ys) x = (fst x, snd x + snd y) : y : ys
-    check :: [(Int, Double)] -> [(Double, Maybe Int)]
+
+    go :: [(Double, Maybe Int)] -> (Int, Double) -> [(Double, Maybe Int)]
+    go [] (x,p) = [(p, Just x)]
+    go (y:ys) x = (snd x + fst y, Just $ fst x) : y : ys
+
+    check :: [(Double, Maybe Int)] -> [(Double, Maybe Int)]
     check [] = [(1, Nothing)]
-    check vals@((_,p):xs)
-      | p < 1 = (1, Nothing) : map (\ (x,y) -> (y, Just x)) vals
-      | p == 1 = map (\ (x,y) -> (y, Just x)) vals
+    check vals@((p, _):xs)
+      | p < 1 = (1, Nothing) : vals
+      | p == 1 = vals
       | otherwise = error "Cumulative probabilities cannot exceed 1"
 
 inverseCdf :: CDF -> Double -> Maybe Int
-inverseCdf (CDF m) n = case n `Map.lookupGE` m of
-                         Just (x, v) -> v
-                         Nothing -> error $ "InverseCDF fails: " ++ show n ++ " CDF: " ++ show m
+inverseCdf (CDF m) n
+  = case n `Map.lookupGE` m of
+      Just (x, v) -> v
+      Nothing -> error $ "InverseCDF fails: " ++ show n ++ " CDF: " ++ show m
 
